8481329169d387264349f49f11d9987a9353c8fe
EdwinRiv <dwein08@gmail.com>
2024-05-20 13:51:49 -0500

    Agregando micro Eureka para el descubrimiento de microservicios, usando server en este micro y eureka cliente en los otros

---
+++ micro-eureka/src/test/java/co/com/coban/microeureka/MicroEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.coban.microeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
